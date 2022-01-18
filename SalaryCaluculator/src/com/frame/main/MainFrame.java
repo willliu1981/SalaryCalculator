15ef0e1d991cea4328d527845626b3033aacc59e
KuanWei <KuanWei@DESKTOP-8R38C1M>
2022-01-19 02:34:23 +0800

    整理 MainFrame

--- SalaryCaluculator/src/com/frame/main/MainFrame.java
+++ SalaryCaluculator/src/com/frame/main/MainFrame.java
@@ -3,40 +3,47 @@ package com.frame.main;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-import com.frame.component.PanelFactory;
 import com.frame.component.behavior.ComponentBehavior;
 import com.frame.component.behavior.TitleComponentBehavior;
-import com.frame.component.tool.Components;
 
 public class MainFrame extends JFrame {
-	private JFrame thisFrame = this;
 
 	private JPanel contentPane;
-	private JPanel panel_content;
+
+	private ComponentBehavior titleComponentBehavior = new TitleComponentBehavior();
 
 	/**
 	 * Create the frame.
 	 */
 	public MainFrame() {
+		initialize();
+	}
+
+	private void initialize() {
+		initMainComponent();
+
+		initContentComponent();
+		initTitleComponent();
+
+		doBehavior();
+	}
+
+	private void initMainComponent() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 999, 603);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+	}
 
-		panel_content = new JPanel();
-		contentPane.add(panel_content, BorderLayout.CENTER);
-		panel_content.setLayout(new CardLayout(0, 0));
-
+	private void initTitleComponent() {
 		JPanel panel_title = new JPanel();
 		contentPane.add(panel_title, BorderLayout.NORTH);
 
@@ -48,10 +55,18 @@ public class MainFrame extends JFrame {
 		btnView.setFont(new Font("新細明體", Font.PLAIN, 36));
 		panel_title.add(btnView);
 
-		ComponentBehavior titleComponentBehavior = new TitleComponentBehavior();
-		titleComponentBehavior.add("panelContent", panel_content);
 		titleComponentBehavior.add("register", btnRegister);
 		titleComponentBehavior.add("view", btnView);
+	}
+
+	private void initContentComponent() {
+		JPanel panel_content = new JPanel();
+		contentPane.add(panel_content, BorderLayout.CENTER);
+		panel_content.setLayout(new CardLayout(0, 0));
+		titleComponentBehavior.add("panelContent", panel_content);
+	}
+
+	private void doBehavior() {
 		titleComponentBehavior.doBehavior();
 	}
 
